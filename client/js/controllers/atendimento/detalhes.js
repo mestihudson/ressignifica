daa0f8e495bbe08cbfc855e1ea93b6c7ffdc3157
Hudson Ferreira Leite <hudson.leite@serpro.gov.br>
2017-01-11 08:09:54 -0200

    corrigido sufixo do age e implementado ng-show para encerrado

--- client/js/controllers/atendimento/detalhes.js
+++ client/js/controllers/atendimento/detalhes.js
@@ -1,6 +1,18 @@
 angular.module("nafavd").controller("AtendimentoDetalhesController", function($scope, atendimento, $filter, questionario) {
   atendimento.data.telefone = $filter("phone")(atendimento.data.telefone);
   atendimento.data.nascimento = $filter("date")(atendimento.data.nascimento, "dd/MM/yyyy");
+
   $scope.atendimento = atendimento.data;
   $scope.questionario = questionario.data;
+
+  var _situacaoByDescricao = function(descricao) {
+    return $scope.questionario.situacao.filter(function(item) {
+      return item.descricao === descricao;
+    })[0];
+  };
+
+  $scope.encerrado = function() {
+    var situacao = $scope.atendimento.questionario.situacao;
+    return !situacao ? false : _situacaoByDescricao("encerramento").id === situacao.id;
+  };
 });
--- client/js/filters/age.js
+++ client/js/filters/age.js
@@ -1,5 +1,5 @@
 angular.module("nafavd").filter("age", function() {
   return function (input, format) {
-    return (format ? moment(input, format) : moment(input)).fromNow(true);
+    return (format ? moment(input, format) : moment(input)).fromNow(true).replace(moment.localeData()._relativeTime.yy.replace("%d", ""), "");
   };
 });
