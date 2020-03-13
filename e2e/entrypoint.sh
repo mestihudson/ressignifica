913be0df4b7f7e0d9421439313825db9a0bccc21
mestihudson <mestihudson@gmail.com>
2020-03-13 15:09:29 -0300

    Increase retries and interval for e2e script

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 which curl > /dev/null || (apk update && apk add curl)
-retries=10
-interval="10s"
+retries="${RETRIES:-10}"
+interval="${INTERVAL:-10s}"
 while [ true ]; do
   if [ $retries -gt 0 ]; then
     result="$(curl -s http://ressignifica-ui:3000 > /dev/null && curl -s http://ressignifica-api:4000 > /dev/null && nc -z ressignifica-db 5432 && curl -s http://ressignifica-hub:4444 > /dev/null && curl -s http://ressignifica-chrome:5555 > /dev/null && curl -s http://ressignifica-firefox:5555 > /dev/null && echo ok || echo fail)"
