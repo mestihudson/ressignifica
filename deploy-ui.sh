5199657c789d58f26a26451ce551ca601486f501
mestihudson <mestihudson@gmail.com>
2020-03-23 18:23:07 -0300

    Config ui deploy on heroku by travisci

---
+++ deploy-ui.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export UI_PATH=.deploy/ui
+export GIT_HEROKU_URL="http://$HEROKU_USERNAME:$HEROKU_API_KEY@git.heroku.com/ressignifica.git"
+git clone $GIT_HEROKU_URL $UI_PATH
+rm -frv $UI_PATH/*
+cp -frv ui/build $UI_PATH/dist
+cp -frv ui/static.json $UI_PATH
+cd $UI_PATH
+git add --all
+git commit -m "Build $TRAVIS_BUILD_NUMBER"
+git push origin master
